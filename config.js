df92025f44de52365b91666030c68059d0685e73
kingranzz <156513688+kingranzz@users.noreply.github.com>
2024-07-07 19:19:05 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ const chalk = require('chalk')
 // ganti info bot dibawah ini
 global.botName = "puki :"
 global.ownerName = "siwa :"
-global.ownerBot = "6282393734303"
-global.ownerNumber = ["6282393734303", "6282320704514"] 
+global.ownerBot = "6282393734303", "6289513665728"
+global.ownerNumber = ["6282393734303", "6289513665728", "6282320704514", "6287811026129", "6285347323656", "62895806422691", "6289681926236", "6281356172466"] 
 
 global.Auto_Typing = false // auto typing
 global.Auto_Recording = false // auto recording
